4bd14baa9ef835ac8ab7fe3219d26d5428606c53
carsakiller <carsakiller@gmail.com>
2023-01-08 23:23:01 -0500

    fix: workspaceOpen state

--- src/commands/workspaceOpen.ts
+++ src/commands/workspaceOpen.ts
@@ -3,5 +3,7 @@ import { useAppStore } from "@/stores/app";
 export default (message: { data: boolean }) => {
   const appStore = useAppStore();
 
+  console.log(message.data);
+
   appStore.workspaceOpen = message.data;
 };
