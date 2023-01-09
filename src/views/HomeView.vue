9cdf7c046c01178b87cc907bf478bcf792c68e6c
carsakiller <carsakiller@gmail.com>
2023-01-09 14:16:33 -0500

    add: show addon totals in tabs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <template>
   <main>
     <vscode-panels>
-      <vscode-panel-tab>BROWSE</vscode-panel-tab>
-      <vscode-panel-tab>INSTALLED</vscode-panel-tab>
+      <vscode-panel-tab
+        >BROWSE<vscode-badge>{{
+          remoteAddonStore.total
+        }}</vscode-badge></vscode-panel-tab
+      >
+      <vscode-panel-tab
+        >INSTALLED<vscode-badge>{{
+          localAddonStore.total
+        }}</vscode-badge></vscode-panel-tab
+      >
       <vscode-panel-view id="browse">
         <BrowseAddons />
       </vscode-panel-view>
@@ -16,16 +24,23 @@
 <script setup lang="ts">
 import BrowseAddons from "./RemoteAddons.vue";
 import InstalledAddons from "./InstalledAddons.vue";
-
+import { useLocalAddonsStore } from "@/stores/localAddons.store";
+import { useRemoteAddonStore } from "@/stores/remoteAddons";
 import {
   provideVSCodeDesignSystem,
+  vsCodeBadge,
   vsCodePanels,
   vsCodePanelTab,
   vsCodePanelView,
 } from "@vscode/webview-ui-toolkit";
+
+const localAddonStore = useLocalAddonsStore();
+const remoteAddonStore = useRemoteAddonStore();
+
 provideVSCodeDesignSystem().register(
   vsCodePanels(),
   vsCodePanelTab(),
-  vsCodePanelView()
+  vsCodePanelView(),
+  vsCodeBadge()
 );
 </script>
